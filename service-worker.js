7348c5c0910d58141db0f87fdd503a27dc530d21
Marcio José Soveral <marcioarteletras@gmail.com>
2025-02-08 02:01:56 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/style.css',
+                '/icon-192x192.png',
+                '/icon-180x180.png',
+                '/icon-120x120.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
