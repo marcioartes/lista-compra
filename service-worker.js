3084360721a1e2257545eddb1d39f8e5f2204106
marcioartes <marcioarteletras@gmail.com>
2025-02-09 01:30:07 -0300

    fix: app,js script path and service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,48 @@
+const CACHE_NAME = 'shopping-list-app-v1';
+const urlsToCache = [
+    '/',
+    '/public/index.html',
+    '/app.js',
+    '/public/style.css',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-180x180.png',
+    '/public/icons/icon-120x120.png'
+];
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/app.js',
-                '/style.css',
-                '/icon-192x192.png',
-                '/icon-180x180.png',
-                '/icon-120x120.png'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
